e8f51f603ab6cf56de1220affff45afde8579319
Genius15066 <amazinggenius72@gmail.com>
2021-03-20 21:32:10 +0600

    destination file add

--- src/components/VehicleDescription/VehicleDescription.js
+++ src/components/VehicleDescription/VehicleDescription.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import'./VehicleDescription.css'
+import fakeData from '../../fakedata/fakedata.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
 
 const VehicleDescription = (props) => {
-    console.log(props.name)
+    const typeVehicle=fakeData.find(pd=>pd.name===props.name)
+    console.log(typeVehicle);
+    const{name,img}=typeVehicle
     return (
-        <div className="d-flex justify-content-around ">
-           <h5>{props.name}</h5>
+        <div className="d-flex justify-content-around mb-4">
+           <img className="vehicle-image" src={img} alt=""/>
+           <h5>{name}</h5>
+           <p><FontAwesomeIcon icon={faUserFriends}/> 6</p>
            <p> $67</p>
         </div>
     );
